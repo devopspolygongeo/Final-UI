4cdea72bca6dee54f73c52661850473acb0a2e85
Vishnu2116 <nvish6965@gmail.com>
2026-02-24 21:54:52 +0530

    mobile restriction added

--- projects/projects/src/app/app.component.ts
+++ projects/projects/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'projects';
+  isMobile = false;
+
+  constructor() {
+    this.updateDeviceView();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.updateDeviceView();
+  }
+
+  private updateDeviceView() {
+    // Treat anything below 1024px as mobile/tablet
+    this.isMobile = window.innerWidth < 1024;
+  }
 }
